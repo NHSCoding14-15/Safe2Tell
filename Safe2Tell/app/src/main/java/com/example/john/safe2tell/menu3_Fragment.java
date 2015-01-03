00bd482f65a4646eaa9672d0f03285b49dfe2ef6
Caleb D <cdud296221@adams12.org>
2015-01-03 16:36:36 -0700

    Added base app and worked on Icon addition

--- Safe2Tell/app/src/main/java/com/example/john/safe2tell/menu3_Fragment.java
+++ Safe2Tell/app/src/main/java/com/example/john/safe2tell/menu3_Fragment.java
@@ -1,130 +1,84 @@
 package com.example.john.safe2tell;
 
-import android.app.FragmentTransaction;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.app.ActionBar;
 
-import com.example.john.safe2tell.R;
+/**
+ * Created by John on 12/30/2014.
+ */
+public class menu3_Fragment extends Fragment {
+    View rootview;
 
-public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
-
-    ViewPager viewPager=null;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        viewPager= (ViewPager) findViewById(R.id.pager);
-        FragmentManager fragmentManager=getSupportFragmentManager();
-        viewPager.setAdapter(new MyAdapter(fragmentManager));
-        final ActionBar actionBar = getActionBar();
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.menu4_layout, container, false);
+        return rootview;
+    }
+}
+
+
+
+
+/*import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup; */
+
+/**
+ * Created by John on 12/30/2014.
+ */
+/*public class menu3_Fragment extends FragmentActivity implements ActionBar.TabListener {
+
+    ActionBar actionBar;
+    View rootview;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.menu3_layout, container, false);
+        return rootview;
+        actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        ActionBar.Tab tab1=actionBar.newTab();
-        tab1.setText("Tab1");
+        ActionBar.Tab tab1= actionBar.newTab();
+        tab1.setText("Tab 1");
         tab1.setTabListener(this);
+
+        ActionBar.Tab tab2= actionBar.newTab();
+        tab1.setText("Tab 2");
+        tab1.setTabListener(this);
+
+        ActionBar.Tab tab3= actionBar.newTab();
+        tab1.setText("Tab 3");
+        tab1.setTabListener(this);
+
+        ActionBar.Tab tab4= actionBar.newTab();
+        tab1.setText("Tab 4");
+        tab1.setTabListener(this);
+
     }
 
     @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
 
     }
 
     @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
     }
 
     @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
     }
 
-
-    /**
-     * Created by John on 12/5/14.
-     */
-
-    //Links class to layout file fragment_a
-    public static class FragmentA extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_a, container, false);
-        }
-    }
-
-    /**
-     * Created by John on 12/5/14.
-     */
-
-    //Links class to layout file fragment_a
-    public static class FragmentB extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_a, container, false);
-        }
-    }
-
-    /**
-     * Created by John on 12/5/14.
-     */
-
-    //Links class to layout file fragment_a
-    public static class FragmentC extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_a, container, false);
-        }
-    }
-
-    /**
-     * Created by John on 12/5/14.
-     */
-
-    //Links class to layout file fragment_a
-    public static class FragmentD extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_d, container, false);
-        }
-    }
-}
-
-class MyAdapter extends FragmentPagerAdapter{
-
-    public MyAdapter(FragmentManager fm) {
-        super(fm);
-    }
-
-    @Override
-    public Fragment getItem(int i) {
-        Fragment fragment=null;
-        if(i==0){
-            fragment=new FragmentA_1();
-        }
-        if(i==1){
-            fragment=new FragmentB_1();
-        }
-        if(i==2){
-            fragment=new FragmentC_1();
-        }
-        if(i==3){
-            fragment=new FragmentD_1();
-        }
-        return fragment;
-    }
-
-    @Override
-    public int getCount() {
-        return (4);
-    }
-
-
-}
+}*/
