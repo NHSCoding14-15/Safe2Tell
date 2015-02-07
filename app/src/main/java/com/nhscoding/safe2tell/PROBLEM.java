a81ee0fd0160ae929cc475f12d5ea8bb42abfb1e
Quantum Matter <davidk@ihcconline.com>
2015-02-06 21:09:13 -0700

    New Working Version
    Reconfigured Navigation Drawer
    Add Problem Selection Field In Action Bar
    Added Problem Fragment
    Added News Fragment
    
    I have the previous version on my computer if anyone needs it.

---
+++ app/src/main/java/com/nhscoding/safe2tell/PROBLEM.java
@@ -0,0 +1,69 @@
+package com.nhscoding.safe2tell;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.app.ActionBar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
+
+import java.lang.reflect.Array;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link PROBLEM.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link PROBLEM#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PROBLEM extends android.support.v4.app.Fragment {
+    View rootview;
+
+    private OnFragmentInteractionListener mListener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.fragment_news, container, false);
+        return rootview;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public static NewsFragment newInstance() {
+        NewsFragment fragment = new NewsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
--- app/src/main/java/com/nhscoding/safe2tell/SettingsClass.java
+++
@@ -1,9 +0,0 @@
-package com.nhscoding.safe2tell;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by John on 2/3/2015.
- */
-public class SettingsClass extends Fragment {
-}
