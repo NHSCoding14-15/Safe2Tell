74b56a9607e49d3fa3131fd3b27def721d893fce
Caleb D <cdud296221@adams12.org>
2015-02-09 09:11:53 -0700

    Added backup cardview into stories page

--- app/src/main/java/com/nhscoding/safe2tell/CustomCard.java
+++ app/src/main/java/com/nhscoding/safe2tell/CustomCard.java
@@ -10,31 +10,41 @@ import android.view.View;
  */
 public class CustomCard extends View {
 
-    public CustomCard(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        TypedArray a = context.getTheme().obtainStyledAttributes(
-                attrs,
-                R.styleable.customCard,
-                0, 0);
-
-        try {
-            mtest = a.getText(R.styleable.customCard_text, false);
-
-        } finally {
-            a.recycle();
-        }
-    }
+//    public CustomCard(Context context, AttributeSet attrs) {
+//        super(context, attrs);
+//        TypedArray a = context.getTheme().obtainStyledAttributes(
+//                attrs,
+//                R.styleable.customCard,
+//                0, 0);
+//
+//        try {
+//            mtest = a.getText(R.styleable.customCard_text, false);
+//
+//        } finally {
+//            a.recycle();
+//        }
+//    }
 
     public CustomCard(Context context) {
         super(context);
     }
 
-    public CustomCard(Context context, AttributeSet attrs) {
-        super(context, attrs);
+//    public CustomCard(Context context, AttributeSet attrs) {
+//        super(context, attrs);
     }
 
-    public CustomCard(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-}
+//    public CustomCard(Context context, AttributeSet attrs, int defStyleAttr) {
+//        super(context, attrs, defStyleAttr);
+//    }
+//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        //Get the width measurement
+//        int widthSize = MeasureUtils.getMeasurement(widthMeasureSpec, getDesiredWidth());
+//
+//        //Get the height measurement
+//        int heightSize = MeasureUtils.getMeasurement(heightMeasureSpec, getDesiredHeight());
+//
+//        //MUST call this to store the measurements
+//        setMeasuredDimension(widthSize, heightSize);
+//    }
+
 
